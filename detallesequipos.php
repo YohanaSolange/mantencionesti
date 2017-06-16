6a9e5de4c9863868469f848625fc58ebbd82a9ea
osvaldoalvaradodev <osvaldo.alvarado.dev@gmail.com>
2017-06-16 17:14:34 -0400

    Detalles Equipos Añadidos

---
+++ detallesequipos.php
@@ -0,0 +1,306 @@
+
+<?php include("header.php"); ?>
+<?php include ("navbar.php"); ?>
+
+
+
+
+
+
+<?php 
+//recibo parametros
+
+$id_equipo = $_GET['id_equipo'];
+
+  include_once("conexion.php");
+              $con2 = new DB;
+              $strConsultaEquipos = "SELECT * FROM `equipos` inner join usuarios on equipos.id_usuario = usuarios.id_usuario inner join tipo_equipo on equipos.id_tipo_equipo = tipo_equipo.id_tipo_equipo where equipos.id_equipo = $id_equipo";
+
+              //echo $strConsultaEquipos;
+
+             $con2->conectar();
+              $buscarUsuariosresultados = mysql_query($strConsultaEquipos);
+             
+
+              $dataset_equipo = mysql_fetch_assoc($buscarUsuariosresultados);
+
+              $nombre_equipo = $dataset_equipo['nombreequipo'];
+
+
+
+
+
+            $con2 = new DB;
+            $strConsultaMantenciones = "SELECT * FROM `mantenciones` left join equipos on mantenciones.id_equipo = equipos.id_equipo left join administradores on mantenciones.id_administrador = administradores.id_administrador left join tipo_mantenciones on tipo_mantenciones.id_tipo_mantencion=mantenciones.id_tipo_mantencion where mantenciones.id_equipo = $id_equipo order by mantenciones.id_mantencion ";
+
+            //echo $strconsultaMantenciones
+
+           // echo $strConsultaMantenciones;
+             $con2->conectar();
+              $buscarMantencionesresultados2 = mysql_query($strConsultaMantenciones);
+              $monto_total_mantenciones = 0;
+               $numregistrosMantenciones2= mysql_num_rows($buscarMantencionesresultados2);
+              for ($i=0; $i<$numregistrosMantenciones2;$i++)
+              {
+                $fila2 = mysql_fetch_array($buscarMantencionesresultados2);
+                $monto_total_mantenciones = $monto_total_mantenciones +    $nombre_admin = $fila2['monto'];
+              }
+?>
+
+
+
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Detalles del Equipo:
+        <small>advanced tables</small>
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="menuprincipal.php"><i class="fa fa-dashboard"></i> Pagina Principal</a></li>
+        <li><a href="registrarequipos.php">Registrar Equipos</a></li>
+       
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+                      <section class="invoice">
+      <!-- title row -->
+      <div class="row">
+        <div class="col-xs-12">
+          <h2 class="page-header">
+            <i class="fa fa-globe"></i> <?php echo $nombre_equipo?>
+            <small class="pull-right">Date: 2/10/2014</small>
+          </h2>
+        </div>
+        <!-- /.col -->
+      </div>
+      <!-- info row -->
+
+
+
+      <div class="row invoice-info">
+
+        <div class="col-sm-3 invoice-col">
+          <div>
+          <address>
+          <strong>Fotografia:</strong><br>
+         
+          <div class="thumbnail" >
+   
+        <img src="img/1.jpg" alt="Lights" style="width:100%">
+       
+     
+    </div>
+          </address>
+
+          </div>
+        </div>
+
+        <div class="col-sm-3 invoice-col">
+         
+          <address>
+<strong>Modelo:</strong><br>
+            <?php echo $dataset_equipo['modelo']?><br>
+             <strong>Usuario:</strong><br>
+            <?php echo "<a href='listadousuarios.php'>".$dataset_equipo['nombre']."</a>"?><br>
+          
+             <strong>Tipo Equipo:</strong><br>
+            <?php echo $dataset_equipo['glosa_tipo_equipo']?><br>
+
+            <strong>Numero de Serie:</strong><br>
+            <?php echo $dataset_equipo['numero_de_serie']?><br>
+              <strong>Mac Equipo:</strong><br>
+              <?php echo $dataset_equipo['mac']?><br>
+         
+          </address>
+        </div>
+        <!-- /.col -->
+        <div class="col-sm-3 invoice-col">
+         
+          <address>
+
+          <strong>Direccion IP:</strong><br>
+                <?php echo $dataset_equipo['IPequipo']?><br>
+
+            <strong>Memoria Ram</strong><br>
+                <?php echo $dataset_equipo['ram']?><br>
+
+                 <strong>Disco Duro</strong><br>
+                <?php echo $dataset_equipo['memoriahdd']?><br>
+             <strong>Procesador</strong><br>
+                <?php echo $dataset_equipo['procesador']?><br>
+          
+             <strong>Sistema Operativo</strong><br>
+                <?php echo $dataset_equipo['so']?><br>
+          </address>
+        </div>
+
+          <div class="col-sm-3 invoice-col">
+         
+          <address>
+            <strong>Estado:</strong><br>
+
+                <?php if ($dataset_equipo['estado'] == 1) {
+                  echo "<span class='badge bg-green'>HABILITADO</span>";
+                }else{
+                  echo "<span class='badge bg-warning'>HABILITADO</span>";
+
+                  }?><br>
+
+                       <strong>Monto Total En Mantenciones:</strong><br>
+                <?php echo "<span class='badge bg-green'>".$monto_total_mantenciones."</span>"?><br>
+               
+          </address>
+        </div>
+
+
+        <!-- /.col -->
+    
+        <!-- /.col -->
+      </div>
+      <!-- /.row -->
+
+      <!-- Table row -->
+      <div class="row">
+        <div class="col-xs-12 table-responsive">
+         <table class="table"  id="tabla1" cellspacing="0" width="100%">
+    <thead>
+  <tr>
+    <th>ID</th>
+    <th>IP</th> 
+    <th>DETALLES</th>
+    <th>CORRECCIONES</th>
+    <th>FECHA</th>
+    <th>PENDIENTES</th>
+ 
+    <th>REGISTRADOR</th>
+     <th class="no-print">TIPO DE MANTENCION</th>
+    <!--<th>ESTADO</th> -->
+  </tr>
+  </thead>
+  <tbody>
+              <?php 
+    
+               $buscarMantencionesresultados = mysql_query($strConsultaMantenciones);
+                   $numregistrosMantenciones= mysql_num_rows($buscarMantencionesresultados);
+
+              if (!$buscarMantencionesresultados) {
+    die("<div class='alert alert-danger'><strong>No se pudo registrar, error:</strong></div> " . mysql_error());
+}else{
+    //no hay errores asi que ejecuta todo esto: 
+
+
+
+              for ($i=0; $i<$numregistrosMantenciones;$i++)
+              {
+              //variable asociativa FILA
+              $fila = mysql_fetch_array($buscarMantencionesresultados);
+              $id_mantenciones= $fila['id_mantencion'];
+              $IP_mantenciones= $fila['IPmantencion'];
+              $fallas_mantenciones= $fila['falla'];
+              $correcciones_mantenciones= $fila['correcciones'];
+              $fecha_mantenciones= $fila['fecha'];
+              $pendientes_mantenciones= $fila['pendiente'];
+              $nombre_usuario = $fila['nombreequipo'];
+              $nombre_admin = $fila['nombre'];
+              $idtipo=$fila['id_tipo_mantencion'];
+              $glosa_tipo_mantencion=$fila['glosa_tipo_mantencion'];
+              echo "<tr>";
+              echo "<td>$id_mantenciones</td>";
+              echo "<td> $IP_mantenciones</td>";
+              echo "<td> $fallas_mantenciones</td>";
+              echo "<td> $correcciones_mantenciones</td>";
+              echo "<td> $fecha_mantenciones</td>";
+              echo "<td> $pendientes_mantenciones</td>";
+             
+              echo "<td> $nombre_admin</td>";
+                   echo "<td class='no-print'> $glosa_tipo_mantencion</td>";
+              echo "</tr>";
+              }
+}
+
+
+              
+             ?>
+     </tbody>
+</table>
+        </div>
+        <!-- /.col -->
+      </div>
+      <!-- /.row -->
+
+      <div class="row">
+        <!-- accepted payments column -->
+        <div class="col-xs-6">
+          <p class="lead">Payment Methods:</p>
+          <img src="../../dist/img/credit/visa.png" alt="Visa">
+          <img src="../../dist/img/credit/mastercard.png" alt="Mastercard">
+          <img src="../../dist/img/credit/american-express.png" alt="American Express">
+          <img src="../../dist/img/credit/paypal2.png" alt="Paypal">
+
+          <p class="text-muted well well-sm no-shadow" style="margin-top: 10px;">
+            Etsy doostang zoodles disqus groupon greplin oooj voxy zoodles, weebly ning heekya handango imeem plugg
+            dopplr jibjab, movity jajah plickers sifteo edmodo ifttt zimbra.
+          </p>
+        </div>
+        <!-- /.col -->
+        <div class="col-xs-6">
+          <p class="lead">Amount Due 2/22/2014</p>
+
+          <div class="table-responsive">
+            <table class="table">
+              <tbody><tr>
+                <th style="width:50%">Subtotal:</th>
+                <td>$250.30</td>
+              </tr>
+              <tr>
+                <th>Tax (9.3%)</th>
+                <td>$10.34</td>
+              </tr>
+              <tr>
+                <th>Shipping:</th>
+                <td>$5.80</td>
+              </tr>
+              <tr>
+                <th>Total:</th>
+                <td>$265.24</td>
+              </tr>
+            </tbody></table>
+          </div>
+        </div>
+        <!-- /.col -->
+      </div>
+      <!-- /.row -->
+
+      <!-- this row will not appear when printing -->
+      <div class="row no-print">
+        <div class="col-xs-12">
+          <a href="invoice-print.html" target="_blank" class="btn btn-default"><i class="fa fa-print"></i> Print</a>
+          <button type="button" class="btn btn-success pull-right"><i class="fa fa-credit-card"></i> Submit Payment
+          </button>
+          <button type="button" class="btn btn-primary pull-right" style="margin-right: 5px;">
+            <i class="fa fa-download"></i> Generate PDF
+          </button>
+        </div>
+      </div>
+    </section>
+
+    
+     
+
+
+      <!-- /.row -->
+    </section>
+    <!-- /.content -->
+  </div>
+
+
+<!-- CAJA DE CONTENIDO CONDENIDO -->
+ 
+
+
+<!-- HASTA AQUI CONTENIDO -->
+
+ <?php include('footer.php');?>
